e03dcc384a210e785a54e9503c4068e0abc9d11e
abdalrheem salah <abdsalahhasan2003@gmail.com>
2025-11-12 17:49:32 +0200

    Add new files

---
+++ Files/src/main/java/com/mycompany/files/Files.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.files;
+
+/**
+ *
+ * @author styde
+ */
+public class Files {
+
+    public static void main(String[] args) {
+  java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Frame().setVisible(true);
+            }
+        });    }
+}
